dbe256d2e5ac184df9f56411a623242e7c55fbe3
Sleep-Ray <lishurui520@outlook.com>
2022-09-19 22:54:14 +0800

    添加制作二维码功能

---
+++ 03generate_qrcode.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project :Daily_Pro 
+@File    :03generate_qrcode.py
+@Author  :李睡睡 主人
+@Date    :2022/9/19 21:41 
+@Scripts :qrcode
+@Function:制作二维码
+@Tips    :宝剑锋从磨砺出，梅花香自苦寒来。
+"""
+import qrcode
+
+input_URL = "https://www.bilibili.com/"
+img_file = r'my_qrcode'
+
+# 实例化QRCode 生成qr对象
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=4,
+)
+
+# 传入数据
+qr.add_data(input_URL)
+
+qr.make(fit=True)
+
+# 生成二维码
+img = qr.make_image(fill_color="blue",back_color="white")
+
+img.save(img_file)
+
+img.show()
